94debd03e8d14b5c8447d4e95112c426354723f6
PraneshS1009 <97275546+PraneshS1009@users.noreply.github.com>
2022-03-01 16:35:29 +0530

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+// function myFunction(){
+//     document.getElementById("date").innerHTML=Date();
+// }
+// function myHtmltojs(){
+//     document.querySelector("p").innerHTML="Hello JavaScript";
+// }
+// var x = document.getElementById("myText").value;
+// var num2=document.getElementById("num2").value;
+
+var name=prompt("Enter yourfull name");
